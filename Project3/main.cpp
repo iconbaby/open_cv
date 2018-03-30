1159e7a7204205360c55a2490e637764a7d81de9
slkk <644187060@qq.com>
2018-03-30 09:23:45 +0800

    first commit

---
+++ Project3/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>  
+#include <opencv2/core/core.hpp>  
+#include <opencv2/highgui/highgui.hpp>  
+
+using namespace cv;
+
+int main()
+{
+	/**/
+	//Mat img = imread("pic.jpg", CV_LOAD_IMAGE_ANYDEPTH | CV_LOAD_IMAGE_ANYCOLOR);
+	//namedWindow("pic");
+	//imshow("ddd",img);
+	//waitKey(6000);
+}
